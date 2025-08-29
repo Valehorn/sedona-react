83b2c3be8a2e3968d84830606aa4d04859a47799
Пакри <pakri@yandex.ru>
2025-08-29 18:44:38 +0300

    Компонент LikeButton

---
+++ src/components/likeButton/likeButton.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function LikeButton({
+  initialCount = 0,
+  onLikeChange,
+  className = '',
+}) {
+  const [likeCount, setLikeCount] = useState(initialCount);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = () => {
+    const newIsLiked = !isLiked;
+    const newCount = newIsLiked ? likeCount + 1 : likeCount - 1;
+
+    setLikeCount(newCount);
+    setIsLiked(newIsLiked);
+
+    if (onLikeChange) {
+      onLikeChange(newCount, newIsLiked);
+    }
+  };
+
+  return (
+    <>
+      <span className={`${className}-counter-container`}>
+        <span className={`${className}-like-counter`}>{likeCount}</span>
+      </span>
+      <button
+        className={`${className}-button ${className}-button--like ${isLiked ? `${className}--liked` : ''}`}
+        onClick={handleClick}
+        type="button"
+        aria-pressed={isLiked}
+      >
+        <span className="visually-hidden">
+          {isLiked ? 'Убрать лайк' : 'Поставить лайк'}
+        </span>
+      </button>
+    </>
+  );
+}
+
+export default LikeButton;
--- src/components/subNavigation/subMenu.jsx
+++ src/components/subNavigation/subMenu.jsx
@@ -1,21 +1,9 @@
 import './subMenu.scss'
 import '../../assets/image/icons/search.svg';
 import '../../assets/image/icons/favorite.svg';
-import { useState } from 'react';
+import LikeButton from '../likeButton/likeButton';
 
 function SubMenu() {
-  const [likeCount, setLikeCount] = useState(12);
-  const [isLiked, setIsLiked] = useState(false);
-
-  const handleLikeClick = () => {
-    if (isLiked) {
-      setLikeCount(prevCount => prevCount - 1);
-    } else {
-      setLikeCount(prevCount => prevCount + 1);
-    }
-    setIsLiked(prev => !prev);
-  };
-
   return (
     <ul className="header__sub-menu-list">
       <li className="header__sub-menu-item">
@@ -24,16 +12,10 @@ function SubMenu() {
         </button>
       </li>
       <li className="header__sub-menu-item">
-        <span className="header__sub-menu-counter-container">
-          <span className="header__sub-menu-like-counter">{likeCount}</span>
-        </span>
-        <button
-          className={`header__sub-menu-button header__sub-menu-button--like ${isLiked ? 'header__sub-menu-button--liked' : ''}`}
-          type="button"
-          onClick={handleLikeClick}
-        >
-          <span className="visually-hidden">Нравится</span>
-        </button>
+        <LikeButton
+          initialCount={12}
+          className="header__sub-menu"
+        />
       </li>
       <li className="header__sub-menu-item">
         <button className="button header__sub-menu-link button--primary" type="button">
